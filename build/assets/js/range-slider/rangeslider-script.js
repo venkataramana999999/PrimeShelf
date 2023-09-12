4f53c73708b16e86b6b9ac7c4c29e05fcaa8b7bf
venkataramana999999 <ramana.m@vmaxindia.com>
2023-09-12 16:42:27 +0530

    latestcodeofprimeshelf

---
+++ build/assets/js/range-slider/rangeslider-script.js
@@ -0,0 +1,78 @@
+'use strict';
+var range_slider_custom = {
+    init: function() { 
+        $("#u-range-01").ionRangeSlider(),
+        $("#u-range-02").ionRangeSlider({
+            min: 100,
+            max: 1000,
+            from: 550
+        }),$("#u-range-03").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 1000,
+            from: 200, 
+            to: 800,
+            prefix: "$"
+        }),$("#u-range-04").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 1000,
+            from: 0,
+            to: 500
+        }), $("#u-range-05").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 1000,
+            from: 0,
+            to: 500,
+            step: 100
+        }), $("#u-range-077").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 100000,
+            from: 0,
+            to: 25000,
+            step: 100
+        }), $("#u-range-088").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 5000,
+            from: 0,
+            to: 500,
+            step: 100
+        }), $("#u-range-055").ionRangeSlider({
+            type: "double",
+            grid: true,
+            min: 0,
+            max: 5000,
+            from: 0,
+            to: 500,
+            step: 100
+        }), $("#u-range-06").ionRangeSlider({
+            grid: true,
+            from: 3,
+            values: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        }), $("#u-range-07").ionRangeSlider({
+            grid: true,
+            min: 1000,
+            max: 10000,
+            from: 2000,
+            step: 100,
+            prettify_enabled: true
+        }), $("#u-range-08").ionRangeSlider({
+            min: 100,
+            max: 1000,
+            from: 550,
+            disable: true
+        });
+    }
+};
+(function($) {
+    "use strict";
+    range_slider_custom.init();
+})(jQuery);
